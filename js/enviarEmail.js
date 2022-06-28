8e5a695ddbd7e253e9f0f72982dedf1aea7711e3
David Cayo <david_dc381@hotmail.com>
2022-06-28 14:07:56 -0400

    Se idento el código en enviarEmail.js

--- js/enviarEmail.js
+++ js/enviarEmail.js
@@ -11,42 +11,42 @@ const d = document,
         e.preventDefault();
         // alert("Se envio el mensaje")
         
-if (nombre.value !== "" && email.value !== "" && mensaje.value !== "") {
-    fetch("https://formsubmit.co/ajax/52cd00cb127682b39d871ef42d3ede4e", {
-      method: "POST",
-      body: new FormData(e.target)
-    })
-      .then(res => res.ok ? res.json(): Promise.reject(res))
-      .then(json => {
-        console.log(json);
-        loader.classList.remove("hide-loader");
-        loader.classList.add("show-loader");
-  
-        setTimeout(() => {
-          enviarForm.reset();
-          loader.classList.add("hide-loader");
-          loader.classList.remove("show-loader");  
-        }, 1500);
-  
-        setTimeout(() => {
-          mensajeEnviado.classList.add("show-mensaje");
-          mensajeEnviado.classList.remove("hide-mensaje");
-        }, 1600);
-  
-        setTimeout(() => {
-          mensajeEnviado.classList.remove("show-mensaje");
-          mensajeEnviado.classList.add("hide-mensaje");
-        }, 4000);
-        
-  
-      })
-      .catch(err => {
-        console.log(err);
-      })
-    
-    } else {
-       return;
-    } 
+        if (nombre.value !== "" && email.value !== "" && mensaje.value !== "") {
+            fetch("https://formsubmit.co/ajax/52cd00cb127682b39d871ef42d3ede4e", {
+              method: "POST",
+              body: new FormData(e.target)
+            })
+              .then(res => res.ok ? res.json(): Promise.reject(res))
+              .then(json => {
+                console.log(json);
+                loader.classList.remove("hide-loader");
+                loader.classList.add("show-loader");
+          
+                setTimeout(() => {
+                  enviarForm.reset();
+                  loader.classList.add("hide-loader");
+                  loader.classList.remove("show-loader");  
+                }, 1500);
+          
+                setTimeout(() => {
+                  mensajeEnviado.classList.add("show-mensaje");
+                  mensajeEnviado.classList.remove("hide-mensaje");
+                }, 1600);
+          
+                setTimeout(() => {
+                  mensajeEnviado.classList.remove("show-mensaje");
+                  mensajeEnviado.classList.add("hide-mensaje");
+                }, 4000);
+                
+          
+              })
+              .catch(err => {
+                console.log(err);
+              })
+            
+            } else {
+              return;
+            } 
       
   });
     
